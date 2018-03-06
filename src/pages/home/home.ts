e35656c8db15e5bc406d7808394a6ce50ce3b183
Anderson <andersonpds14@gmail.com>
2018-03-06 18:30:27 -0300

    Adicionado função para calcular estimativa de altura

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,13 +10,29 @@ export class HomePage {
   tituloBotao = 'Calcular Altura'
   idade = 0
   comprimentoPerna = 0
-  sexo
+  sexo = null
+  alturaEstimada = 0
   constructor(public navCtrl: NavController) {
     
+  }
+
+  calcularAlturaEstimada(){
+    if(this.sexo != null){
+      if(this.sexo == "Masculino"){
+        this.alturaEstimada = (2.02 * this.comprimentoPerna) - (0.04 * this.idade) + 64.19;
+      } else{
+        this.alturaEstimada = (1.83 * this.comprimentoPerna) - (0.24 * this.idade) + 84.88;
+      }
+    }
   }
 
   logEvent(){
     console.log("Idade: " + this.idade + ". Comprimento Perna: " + this.comprimentoPerna + "Sexo: " + this.sexo);
   }
 
+  limparCampos(){
+    this.idade = 0
+    this.comprimentoPerna = 0
+    this.sexo = ''
+  }
 }
